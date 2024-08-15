21bf550298f087339fee95a48bb0a5220c50c4d7
kritikamalviya <kritikamalviya123@gmail.com>
2024-08-15 17:08:21 +0530

    Demo of static block, method and variable

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Java-Static-and-Non-Static
+
+1. Static Block
+2. Static Method
+3. Static Variable
---
+++ statics/src/Main.java
@@ -0,0 +1,21 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Inside Main method");
+
+    }
+
+    //Static blocks
+
+    static {
+        System.out.println("Inside static block 1");
+    }
+
+    static {
+        System.out.println("Inside static block 2");
+    }
+
+}
